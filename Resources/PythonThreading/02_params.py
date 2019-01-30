9b69780fc9ee83480aa61ea2299c76958e11ca31
rugbyprof <terry.griffin@mwsu.edu>
2019-01-30 15:50:53 -0600

    Pushed from BO128B-49502

---
+++ Resources/PythonThreading/02_params.py
@@ -0,0 +1,10 @@
+import threading
+
+def f(id):
+    print 'thread function %s' %(id)
+    return
+
+if __name__ == '__main__':
+    for i in range(3):
+        t = threading.Thread(target=f, args=(i,))
+        t.start()
---
+++ Resources/os_pages/README.md
@@ -0,0 +1,1 @@
+## Resources os_pages - No Description
