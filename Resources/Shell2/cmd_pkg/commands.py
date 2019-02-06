d450c5c3acbce4fbcf03b903cc42b2879f1b7a89
rugbyprof <terry.griffin@mwsu.edu>
2019-02-05 18:26:09 -0600

    Pushed from BO128B-49502

--- Resources/Shell2/cmd_pkg/commands.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from ls import ls
-from pwd import pwd
-from cat import cat
-from grep import grep
-from grep import grepusage
-
-if __name__=='__main__':
-  pass
--- Resources/Shell2/cmd_pkg/ls.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from subprocess import call
-
-def ls(flags):
-    call(["ls", flags])
-
