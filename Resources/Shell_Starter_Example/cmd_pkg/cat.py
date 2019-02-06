1a35cd7b3bdee35945157500c307288d7aa4d253
Terry Griffin <terry.griffin@msutexas.edu>
2019-02-06 16:40:38 -0600

    Pushed from Terrys-MacBook-Pro

--- Resources/Shell_Starter_Example/cmd_pkg/cat.py
+++ Resources/Shell_Starter_Example/cmd_pkg/cat.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 from subprocess import call
 
-def cat(file):
+cat('params'=['README.md','bacons.txt'],'flags'=[] 'appendfile'='outname.txt')
+
+def cat(**kwargs):
+    string_Files = ""
+    if 'params' in kwargs:
+        if len(kwargs['params'] == 0):
+            catusage()
+            return
+        else:
+            for f in kwargs['params']:
+            f = open(f,'r').read()
+
+            return string_Files
+
     call(["cat", file])
