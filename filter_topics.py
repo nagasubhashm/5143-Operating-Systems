b33bfb0abe46106d2172a1eb5897c1f4933d2a8a
rugbyprof <terry.griffin@mwsu.edu>
2020-02-24 08:12:14 -0600

    Pushed from BO126D-49502

--- filter_topics.py
+++ filter_topics.py
@@ -1,4 +1,4 @@
-fp = open("topics.txt","r")
+fp = open("topics.txt", "r")
 
 lines = fp.readlines()
 topics = []
@@ -8,10 +8,9 @@ print(len(lines))
 for l in lines:
     l = l.strip()
     if '-' in l:
-        l,j = l.split('-')
+        l, j = l.split('-')
     if not l in topics:
         topics.append(l)
-
 
 for t in topics:
     print(t)
